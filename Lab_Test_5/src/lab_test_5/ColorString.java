c2c14241a8e20755dad65baffc6c5deeccae5715
Atul Kumar Mishra <101014298+AtulMishra258@users.noreply.github.com>
2023-02-02 18:09:52 +0530

    Add files via upload

---
+++ Lab_Test_5/src/lab_test_5/ColorString.java
@@ -0,0 +1,13 @@
+package lab_test_5;
+import java.util.ArrayList;
+
+public class ColorString {
+	public static void main(String[] args) {
+		ArrayList<String> ans= new ArrayList<String>();
+		ans.add("green");
+		ans.add("blue");
+		ans.add("black");
+		ans.add("yello");
+		System.out.println(ans);
+	}
+}
---
+++ Lab_Test_5/src/lab_test_5/Iterate.java
@@ -0,0 +1,21 @@
+package lab_test_5;
+
+import java.util.TreeSet;
+
+public class Iterate {
+
+	public static void main(String[] args) {
+		TreeSet<String> tree_set = new TreeSet<String>();
+		  tree_set.add("Red");
+		  tree_set.add("Green");
+		  tree_set.add("Orange");
+		  tree_set.add("White");
+		  tree_set.add("Black");
+		 
+		  for (String element : tree_set) {
+		    System.out.println(element);
+		    }
+
+	}
+
+}
